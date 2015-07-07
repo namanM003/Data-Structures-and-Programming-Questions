26a0b6079306dcb929e4e97672c6f7af488bed12
Naman Mittal <namanmittal003@gmail.com>
2015-07-07 15:24:36 -0400

    Invert tree

---
+++ invertTree.java
@@ -0,0 +1,24 @@
+/* Invert a binary tree. */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        //invertTree(root.left);
+        //invertTree(root.right);
+        return root;
+    }
+}
