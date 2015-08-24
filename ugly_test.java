249f45011430d44d0e1494d89ce21a0d77eea215
Naman Mittal <namanmittal003@gmail.com>
2015-08-23 23:48:20 -0400

    Check number is ugly or not

---
+++ ugly_test.java
@@ -0,0 +1,24 @@
+/*Check whether a given no is ugly or not*/
+public class Solution {
+    public boolean isUgly(int num) {
+        if(num==0){
+            return false;
+        }
+        while(num!=1 && (num%2==0 || num%5==0 || num%3==0)){
+            if(num%2==0){
+                num = num/2;
+            }
+            if(num%3==0){
+                num = num/3;
+            }
+            if(num%5==0)
+                num = num/5;
+        }
+        if(num==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
